33c44921660ee8a33515e16c4ea4bd83a83c3bc2
Ice <gpreciousalyssa@gmail.com>
2023-11-13 23:00:14 -0600

    Implement reverseString in one line

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,20 +1,5 @@
 const reverseString = function(string) {
-  const arr = string.split(' ');
-  const newArr = []
-
-  for(let i=0; i<arr.length; i++) {
-    const wordLength = arr[i].length - 1;
-    let word = '';
-
-    for(let j=wordLength; j>=0; j--){
-      word += arr[i][j];
-    };
-    
-    newArr.unshift(word)
-
-  };
-
-  return newArr.join(' ')
+  return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
