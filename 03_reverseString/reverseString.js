11a9c67a1bacc81d9e645e12af269742e5289d93
Ice <gpreciousalyssa@gmail.com>
2023-11-13 20:09:47 -0600

    Pass reverseString Test Suite

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,20 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  const arr = string.split(' ');
+  const newArr = []
 
+  for(let i=0; i<arr.length; i++) {
+    const wordLength = arr[i].length - 1;
+    let word = '';
+
+    for(let j=wordLength; j>=0; j--){
+      word += arr[i][j];
+    };
+    
+    newArr.unshift(word)
+
+  };
+
+  return newArr.join(' ')
 };
 
 // Do not edit below this line
