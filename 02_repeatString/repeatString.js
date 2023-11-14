8d927689caa67b18409b2ccbfda291f5f34ea3ec
Ice <gpreciousalyssa@gmail.com>
2023-11-13 19:15:20 -0600

    Pass repeatString Test Suite

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  if (num<0) {return 'ERROR'};
 
+  newString = ''
+  for (i=0; i<num; i++) {
+    newString = newString.concat(string);
+  };
+
+  return newString;
 };
 
 // Do not edit below this line
