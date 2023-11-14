59baf7cc2a5f91b57a89f4ef4ceea90811333478
Ice <gpreciousalyssa@gmail.com>
2023-11-13 19:32:55 -0600

    Skip For loop for blank string input

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,14 @@
 const repeatString = function(string, num) {
-  if (num<0) {return 'ERROR'};
-
-  newString = ''
-  for (i=0; i<num; i++) {
-    newString = newString.concat(string);
+  if (num<0) return 'ERROR';
+  else if (string == '') return '';
+  else {
+    let newString = ''
+    for (i=0; i<num; i++) {
+      newString = newString.concat(string);
+    };
+  
+    return newString;
   };
-
-  return newString;
 };
 
 // Do not edit below this line
