311243500a7c92fcca50d064a3a33fe7096cb867
Ice <gpreciousalyssa@gmail.com>
2023-11-15 19:19:57 -0600

    Pass tempConversion Test Suite

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  if (tempF == 32) return 0;
+  tempC = 5/9 * (tempF - 32);
+  return Number(tempC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  if (tempC == 0) return 32;
+  tempF = tempC * 9/5 + 32
+  return Number(tempF.toFixed(1));
 };
 
 // Do not edit below this line
